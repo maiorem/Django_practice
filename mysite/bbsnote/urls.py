85e5bf521b73f7254dbc8514143906e736c2872c
Seyoung Hong <maiorem00@gmail.com>
2021-03-20 17:04:03 +0900

    comment

--- mysite/bbsnote/urls.py
+++ mysite/bbsnote/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'bbsnote'
+
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('<int:board_id>/', views.detail, name='detail'),
+    path('comment/create/<int:board_id>/', views.comment_create, name='comment_create'),
 ]
--- mysite/bbsnote/views.py
+++ mysite/bbsnote/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, Comment
+from django.utils import timezone
 
 # Create your views here.
 def index(request) :
@@ -8,3 +9,16 @@ def index(request) :
     context = {'board_list' : board_list }
     return render(request, 'bbsnote/board_list.html', context)
     # return HttpResponse("bbsnote에 오신 것을 환영합니다.");
+
+def detail(request, board_id) :
+    board = Board.objects.get(id=board_id)
+    context = {'board' : board}
+    return render(request, 'bbsnote/board_detail.html', context)
+
+def comment_create(request, board_id) :
+    board = Board.objects.get(id=board_id)
+    # comment = Comment(board=board, content=request.POST.get('content'), create_date=timezone.now())
+    # comment.save()
+    # Board 모델과 Comment 모델이 Foreign key로 묶여 있는 경우 _set으로 해주면 자동으로 표현이 됨
+    board.comment_set.create(content=request.POST.get('content'), create_date=timezone.now()) 
+    return redirect('bbsnote:detail', board_id=board.id)
